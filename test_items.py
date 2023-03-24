b233cba0b6278f2512b48698dbeabafdc90d699b
Dzmitry Lapitski <dz.lap.pro@gmail.com>
2023-03-24 23:29:11 +0100

    Slight changes to the assertion logic.

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_add_to_basket_button_exists(browser):
     browser.implicitly_wait(5)
     browser.get(link)
-    add_to_basket = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket1')
+    add_to_basket = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
     assert len(add_to_basket) > 0, 'Could NOT find the "Add to basket" button'
     time.sleep(5)  # To check the correctness of the set language
