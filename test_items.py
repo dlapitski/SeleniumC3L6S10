d150919993a806d97ed608c8c3281fca55ecf046
Dzmitry Lapitski <dz.lap.pro@gmail.com>
2023-03-24 12:59:50 +0100

    Initial commit with the next files: .gitignore conftest.py test_items.py

---
+++ test_items.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_add_to_basket_button_exists(browser):
+    browser.implicitly_wait(5)
+    browser.get(link)
+    add_to_basket = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
+    assert add_to_basket[0], 'Could NOT find the "Add to basket" button'
+    time.sleep(5)  # To check the correctness of the set language
